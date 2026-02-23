52fcc19391c8ae8156119bda29b9ca105acace1e
Manish Rai <manishrai9663@gmail.com>
2026-02-23 14:23:54 +0530

    "Delete backend directory"
    Trying out fresh one

--- backend/app/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel,EmailStr,Field
-
-class LeadCreate(BaseModel):
-    name : str = Field(...,min_lenght=2,max_length=25)
-    email : EmailStr
-    phone: str = Field(...,min_length=10,max_length=15)
-    source:str
